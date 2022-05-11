d95983b43693de28a917dba68712a795d5881dcb
2Nonku123 <t33tndlovu@gmail.com>
2022-05-11 15:56:13 +0200

    next commit

--- shop/shopping-cart.js
+++ shop/shopping-cart.js
@@ -1,25 +1,53 @@
-function ShoppingCart (pool){
-	
-	function addToCart(userId, itemId) {
+module.exports = function ShoppingCart(pool) {
+  async function addCart(userId) {
+    const result = await pool.query(
+      `INSERT INTO cart(user_id,status) VALUES($1 ,$2); `,
+      ["open", userId]
+    );
 
-	}
+    return result.rowCount;
+  }
+  async function addToCart(cartId, itemId, qty) {
+    const result = await pool.query(
+      `INSERT INTO shopping_cart(status,cart_id,garment_id,qty) VALUES($1,$2,$3,$4); `,
+      ["added", cartId, itemId, qty]
+    );
 
-	function getCart(userId) {
+    return result.rowCount;
+  }
 
-	}
+  async function getCart(userId) {
+    const result = await pool.query(
+      `select * from cart where user_id= $1 AND status = 'open'`,
+      [userId]
+    );
 
-	function carts(userId){
+	if(result.)
 
-	}
+    return result.rows;
+  }
 
-	function checkOut(cartId) {
-		
-	}
+  async function carts(userId) {
+    const result = await pool.query(`select * from cart where user_id = $1`, [
+      userId,
+    ]);
+    return result.rows;
+  }
 
-	return {
-		addToCart,
-		getCart,
-		carts
-	}
+  async function checkOut(cartId) {
+    const result = await pool.query(
+      `UPDATE cart SET status =$1 WHERE id = $2`,
+      ["completed", cartId]
+    );
 
-}
+    return result.rowCount;
+  }
+
+  return {
+    addToCart,
+    getCart,
+    carts,
+    checkOut,
+    addCart,
+  };
+};
